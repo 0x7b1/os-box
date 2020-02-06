c6e2cc5447d892980ab14767b709e96824d3e4e8
OliverSuik <oliversuik@gmail.com>
2020-02-06 18:32:53 +0200

    Fonts and style correction

--- src/components/app/tab-details.js
+++ src/components/app/tab-details.js
@@ -6,6 +6,7 @@ import Button from '@jetbrains/ring-ui/components/button/button';
 import ButtonSet from '@jetbrains/ring-ui/components/button-set/button-set';
 import Dropdown from '@jetbrains/ring-ui/components/dropdown/dropdown';
 import PopupMenu from '@jetbrains/ring-ui/components/popup-menu/popup-menu';
+
 import {
   PinFilledIcon,
   PinEmptyIcon,
@@ -48,7 +49,6 @@ export default class TabDetails extends Component {
     ];
 
     const result = [];
-    const isEditMode = false;
 
     data.forEach(({ category, items }, i) => {
       result.push({
@@ -56,60 +56,18 @@ export default class TabDetails extends Component {
         label: `${category} (${items.length})`,
       });
 
-      items.forEach(({ name, description, pinned }) => {
+      items.forEach(({ name, description }) => {
         result.push({
           label: (
             <Group>
               <Text>{name}</Text>
               <Text info>{description}</Text>
-              {
-                pinned && (
-                  <PinFilledIcon
-                    className="ring-icon"
-                    color={PinFilledIcon.Color.GRAY}
-                    size={PinFilledIcon.Size.Size16}
-                  />
-                )
-              }
             </Group>
           ),
           rgItemType: List.ListProps.Type.ITEM,
-          description: (
-            <div>
-              <ButtonSet>
-                <Button icon={LinkIcon} />
-                {/* <Button icon={CommentIcon} /> */}
-                {/* <Button icon={AttachmentIcon} /> */}
-              </ButtonSet>
-              <Dropdown
-                anchor={<Button icon={MoreOptionsIcon} />}
-              >
-                <PopupMenu
-                  closeOnSelect
-                  data={[
-                    { label: <Button icon={PinEmptyIcon}>Unpin</Button> },
-                    { label: <Button icon={PencilIcon}>Edit</Button> },
-                    { label: <Button icon={TrashIcon}>Remove</Button> },
-                  ]}
-                />
-              </Dropdown>
-            </div>
-          ),
-          disabled: true,
+
         });
       });
-
-      if (isEditMode) {
-        result.push({
-          label: <Button short text delayed>{'Add New Item'}</Button>,
-          rgItemType: List.ListProps.Type.ITEM,
-          disabled: true
-        });
-
-        result.push({
-          rgItemType: List.ListProps.Type.SEPARATOR
-        });
-      }
     });
 
     return result;
